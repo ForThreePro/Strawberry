85dcb13ed7ffe5bcf771b34464aed5fb5c64b24d
YALLICO OWNER <yallico2024@gmail.com>
2026-07-09 23:34:58 -0500

    Actualizar enable.js

--- plugins/enable.js
+++ plugins/enable.js
@@ -15,6 +15,10 @@ let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isR
       if (m.isGroup &&!isAdmin) { global.dfail('admin', m, conn); fail = true; break }
       chat.bienvenida = isEnable
       break
+    case 'detector':
+      if (m.isGroup &&!isAdmin) { global.dfail('admin', m, conn); fail = true; break }
+      chat.detector = isEnable
+      break
     case 'subbots': case 'serbot':
       if (!isROwner) { global.dfail('rowner', m, conn); fail = true; break }
       bot.jadibotmd = isEnable
@@ -82,8 +86,8 @@ let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isR
   }
 }
 
-handler.help = ['welcome', 'antilink', 'antibot', 'modoadmin', 'subbots', 'nsfw', 'audios', 'antiprivado'].map(v => v + ' on/off')
+handler.help = ['welcome', 'detector', 'antilink', 'antibot', 'modoadmin', 'subbots', 'nsfw', 'audios', 'antiprivado'].map(v => v + ' on/off')
 handler.tags = ['config']
-handler.command = ['welcome', 'bienvenida', 'subbots', 'serbot', 'antispam', 'antilink', 'antibot', 'modoadmin', 'nsfw', 'antinopor', 'audios', 'autoleer', 'autoread', 'antiprivado']
+handler.command = ['welcome', 'bienvenida', 'detector', 'subbots', 'serbot', 'antispam', 'antilink', 'antibot', 'modoadmin', 'nsfw', 'antinopor', 'audios', 'autoleer', 'autoread', 'antiprivado']
 
 export default handler
