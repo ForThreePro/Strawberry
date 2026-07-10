bf1633bc9db80a43c2899af9960ab737a8a6c889
YALLICO OWNER <yallico2024@gmail.com>
2026-07-09 22:23:29 -0500

    Actualizar group-banchat-unbanchat.js

--- plugins/group-banchat-unbanchat.js
+++ plugins/group-banchat-unbanchat.js
@@ -10,15 +10,15 @@ let handler = async (m, { conn, isOwner, isAdmin, isROwner, command }) => {
 
   switch (type) {
     case 'banchat': case 'banearchat':
-      if (chat.isBanned) return m.reply(`🛸 *[ BOX BOT MD ]* 🌌\n\n⚠️ *Este chat ya se encuentra baneado.*`)
+      if (chat.isBanned) return m.reply(`⛈️ *RAYO PREM* 🌙\n\n⚡ *Este chat ya se encuentra baneado.*`) // Cambiado
       chat.isBanned = true
-      await conn.reply(m.chat, `🛸 *[ BOX BOT MD ]* 🌌\n\n🚫 *Chat Baneado:* El bot ha sido desactivado en este grupo.\n💬 No responderé a ningún comando hasta que sea desbloqueado.\n\n⚙️ *Box Bot MD • Control de Grupos* 🌀`, m)
+      await conn.reply(m.chat, `⛈️ *RAYO PREM* ➔ CHAT BANEADO 🌙\n\n🌩️ *El bot ha sido desactivado en este grupo.*\n⚡ No responderé a ningún comando hasta que sea desbloqueado.\n\n⛈️ *Team Nightwish*`, m) // Cambiado
       break
 
     case 'unbanchat': case 'desbanearchat':
-      if (!chat.isBanned) return m.reply(`🛸 *[ BOX BOT MD ]* 🌌\n\n⚠️ *Este chat no está baneado.*`)
+      if (!chat.isBanned) return m.reply(`⛈️ *RAYO PREM* 🌙\n\n⚡ *Este chat no está baneado.*`) // Cambiado
       chat.isBanned = false
-      await conn.reply(m.chat, `🛸 *[ BOX BOT MD ]* 🌌\n\n🌀 *Chat Desbaneado:* El bot vuelve a estar activo en este grupo.\n⚡ Ya pueden utilizar todos los comandos con normalidad.\n\n⚙️ *Box Bot MD • Control de Grupos* 🌀`, m)
+      await conn.reply(m.chat, `⚡ *RAYO PREM* ➔ CHAT DESBANEADO 🌙\n\n🌩️ *El bot vuelve a estar activo en este grupo.*\n⚡ Ya pueden utilizar todos los comandos con normalidad.\n\n⛈️ *Team Nightwish*`, m) // Cambiado
       break
 
     default:
