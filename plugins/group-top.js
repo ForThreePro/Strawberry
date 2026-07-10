aa9c880ac5cc2ec36e65aea8d8a1147dedbf1e4d
YALLICO OWNER <yallico2024@gmail.com>
2026-07-09 21:12:18 -0500

    Actualizar group-top.js

--- plugins/group-top.js
+++ plugins/group-top.js
@@ -2,7 +2,7 @@ import util from 'util'
 import path from 'path'
 let user = a => '@' + a.split('@')[0]
 function handler(m, { groupMetadata, command, conn, text, usedPrefix}) {
-if (!text) return m.reply(`Ejemplo de uso:\n.top *texto*`)
+if (!text) return m.reply(`⛈️ *RAYO PREM TOP* 🌙\n\n⚡ *Ejemplo:*.top gente mas activa`) // Cambiado
 let ps = groupMetadata.participants.map(v => v.id)
 let a = ps.getRandom()
 let b = ps.getRandom()
@@ -15,21 +15,20 @@ let h = ps.getRandom()
 let i = ps.getRandom()
 let j = ps.getRandom()
 let k = Math.floor(Math.random() * 70);
-let x = `${pickRandom(['🤓','😅','😂','😳','😎', '🥵', '😱', '🤑', '🙄', '💩','🍑','🤨','🥴','🔥','👇🏻','😔', '👀','🌚','🗿'])}`
-let l = Math.floor(Math.random() * x.length);
+let x = `${pickRandom(['⛈️','🌩️','⚡','🌙','🔥','💀','😎','🤙','👑','💥'])}` // Cambiado emojis
 let vn = `https://hansxd.nasihosting.com/sound/sound${k}.mp3`
-let top = `*${x} Top 10 ${text} ${x}*
-    
-*1. ${user(a)}*
-*2. ${user(b)}*
-*3. ${user(c)}*
-*4. ${user(d)}*
-*5. ${user(e)}*
-*6. ${user(f)}*
-*7. ${user(g)}*
-*8. ${user(h)}*
-*9. ${user(i)}*
-*10. ${user(j)}*`
+let top = `⛈️ *RAYO PREM* ➔ TOP 10 ${text.toUpperCase()} 🌙\n\n` // Cambiado
+top += `⚡ *1.* ${user(a)}\n`
+top += `⚡ *2.* ${user(b)}\n`
+top += `⚡ *3.* ${user(c)}\n`
+top += `⚡ *4.* ${user(d)}\n`
+top += `⚡ *5.* ${user(e)}\n`
+top += `⚡ *6.* ${user(f)}\n`
+top += `⚡ *7.* ${user(g)}\n`
+top += `⚡ *8.* ${user(h)}\n`
+top += `⚡ *9.* ${user(i)}\n`
+top += `⚡ *10.* ${user(j)}\n\n`
+top += `🌙 *Team Nightwish*` // Cambiado
 m.reply(top, null, { mentions: [a, b, c, d, e, f, g, h, i, j]})
 }
 handler.help = handler.command = ['top']
