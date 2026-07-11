d2968fa3dbedb7ce31538fd6f905faa81c226af6
YALLICO OWNER <yallico2024@gmail.com>
2026-07-11 10:23:35 -0500

    Actualizar owene-autoadmin.js

--- plugins/owene-autoadmin.js
+++ plugins/owene-autoadmin.js
@@ -1,9 +1,19 @@
 let handler = async (m, { conn }) => {
     try {
         await conn.groupParticipantsUpdate(m.chat, [conn.user.jid], 'promote')
-        m.reply('⚡ *RAYO PREM* ➔ Admin asignado. El trueno toma el control.') // Cambiado
+        m.reply(`╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
+│ 👑 *AUTOPROMOTE*
+│
+│ ✅ *Estado:* Admin asignado
+│ ⚡ *El trueno toma el control*
+╰─────────────────❒`)
     } catch (e) {
-        m.reply('⚡ *RAYO PREM ERROR* ➔ No pude asignarme admin. Dame permisos primero.') // Cambiado
+        m.reply(`╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
+│ ❌ *ERROR AUTOPROMOTE*
+│
+│ ⚠️ *No pude asignarme admin*
+│ 🌙 *Dame permisos primero*
+╰─────────────────❒`)
     }
 }
 
