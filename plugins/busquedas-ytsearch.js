b3657e9a6ec6789d3fdcb18ad6d2c1d76ec7e67a
YALLICO OWNER <yallico2024@gmail.com>
2026-07-12 15:01:48 -0500

    Actualizar busquedas-ytsearch.js

--- plugins/busquedas-ytsearch.js
+++ plugins/busquedas-ytsearch.js
@@ -1,27 +1,27 @@
 import axios from 'axios'
 let handler = async (m, { conn, text }) => {
-    if (!text) return m.reply('⛈️ *RAYO PREM YT SEARCH* 🌙\n\n⚡ *¿Qué deseas buscar en YouTube?*') // Cambiado
+    if (!text) return m.reply('🍓 *BOT STRAWBERRY YT SEARCH* 🍓\n\n💖 *¿Qué deseas buscar en YouTube?*') 
     await m.react('📺')
     try {
         let { data } = await axios.get(`https://api.delirius.store/search/ytsearch?q=${encodeURIComponent(text)}`)
-        if (!data.data || data.data.length === 0) return m.reply(`⛈️ *RAYO PREM ERROR* ➔ *No se encontraron resultados.*`) // Cambiado
+        if (!data.data || data.data.length === 0) return m.reply(`🍓 *BOT STRAWBERRY ERROR* ➔ *No se encontraron resultados.*`) 
 
         let res = data.data.slice(0, 5).map((v, i) => 
-            `🌩️ *${i+1}.* *${v.title}*\n` +
+            `🍓 *${i+1}.* *${v.title}*\n` +
             `⏳ *Duración:* ${v.duration} | 👁️ *Vistas:* ${v.views}\n` +
             `👤 *Canal:* ${v.author.name}\n` +
             `🔗 ${v.url}`
         ).join('\n\n')
 
-        let cap = `⛈️ *RAYO PREM YOUTUBE SEARCH* 🌙\n\n` // Cambiado
+        let cap = `🍓 *BOT STRAWBERRY YOUTUBE SEARCH* 🍓\n\n` 
         cap += res
-        cap += `\n\n⚡ *Usa:* .play <número> o copia el link para descargar\n⛈️ *Team Nightwish*` // Cambiado
+        cap += `\n\n💖 *Usa:* .play <número> o copia el link para descargar\n🍓 *Bot Strawberry*` 
 
         m.reply(cap)
         await m.react('✅')
     } catch { 
         await m.react('❌')
-        m.reply(`⛈️ *RAYO PREM ERROR* ➔ *Error al buscar.*`) // Cambiado
+        m.reply(`🍓 *BOT STRAWBERRY ERROR* ➔ *Error al buscar.*`) 
     }
 }
 handler.help = ['yts <busqueda>']
