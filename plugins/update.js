bcc7d0bfef10d174b713097f2920ce88d444a620
YALLICO OWNER <yallico2024@gmail.com>
2026-07-12 16:55:10 -0500

    Actualizar update.js

--- plugins/update.js
+++ plugins/update.js
@@ -1,25 +1,25 @@
 import { exec } from "child_process"
 
 const handler = async (m, { conn }) => {
-    const owner = "👑 *Creador: Whois Yallico*"
+    const owner = "👑 *Creador: Whois Yallico 💖*"
 
-    if (m.react) await m.react('⚡')
+    if (m.react) await m.react('⏳')
 
-    await conn.reply(m.chat, '⛈️ *RAYO PREM* ➔ Sincronizando con el trueno... Actualizando módulos.', m)
+    await conn.reply(m.chat, '🍓 *BOT STRAWBERRY* ➔ Sincronizando con la canasta... Actualizando módulos.', m)
 
     exec('git pull', async (err, stdout, stderr) => {
         if (err) {
             if (m.react) await m.react('❌')
-            return conn.reply(m.chat, `⚡ *RAYO PREM ERROR* ➔ Falló la actualización.\n\n\`\`${err.message}\`\n\n${owner}`, m)
+            return conn.reply(m.chat, `🍓 *BOT STRAWBERRY ERROR* ➔ Falló la actualización.\n\n\`\`${err.message}\`\`\n\n${owner}`, m)
         }
 
         if (stdout.includes('Already up to date.')) {
-            if (m.react) await m.react('🌙')
-            return conn.reply(m.chat, `⚡ *RAYO PREM* ➔ El sistema ya está en su versión más reciente.\n\n${owner}\n> "El trueno nunca duerme"`, m)
+            if (m.react) await m.react('✅')
+            return conn.reply(m.chat, `🍓 *BOT STRAWBERRY* ➔ El sistema ya está en su versión más reciente.\n\n${owner}\n> "Las fresas nunca duermen" 🍓`, m)
         }
 
-        if (m.react) await m.react('⚡')
-        return conn.reply(m.chat, `⚡ *RAYO PREM* ➔ Actualización aplicada con éxito.\n\n*📜 Cambios:*\n\`\`\`${stdout}\`\`\`\n\n${owner}`, m)
+        if (m.react) await m.react('✅')
+        return conn.reply(m.chat, `🍓 *BOT STRAWBERRY* ➔ Actualización aplicada con éxito.\n\n*📜 Cambios:*\n\`\`${stdout}\`\n\n${owner}`, m)
     })
 }
 
