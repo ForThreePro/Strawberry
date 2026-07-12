9489dca82c91a9e7a7c96708fec832e601e20587
YALLICO OWNER <yallico2024@gmail.com>
2026-07-12 14:59:54 -0500

    Actualizar busquedas-googlesearch.js

--- plugins/busquedas-googlesearch.js
+++ plugins/busquedas-googlesearch.js
@@ -1,12 +1,11 @@
 import axios from 'axios'
 
 let handler = async (m, { conn, text }) => {
-    if (!text) return m.reply(`╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
-│ 🔍 *GOOGLE SEARCH*
-│
-│ 🌙 *¿Qué deseas buscar?*
-│ ⚡ *Uso:*.google Team Nightwish
-╰─────────────────❒`)
+    if (!text) return m.reply(`🍓 *『 𝐁𝐎𝐓 𝐒𝐓𝐑𝐀𝐖𝐁𝐄𝐑𝐘 』* 🍓
+💖 *GOOGLE SEARCH*
+
+🍓 *¿Qué deseas buscar?*
+💖 *Uso:*.google Bot Strawberry`)
 
     await m.react('🔍')
 
@@ -14,27 +13,24 @@ let handler = async (m, { conn, text }) => {
         let { data } = await axios.get(`https://api.delirius.store/search/google?query=${encodeURIComponent(text)}`)
         let results = data.data.slice(0, 5)
 
-        if (!results.length) return m.reply(`╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
-│ ❌ *SIN RESULTADOS*
-│
-│ ⚡ *No se encontró nada sobre:* ${text}
-╰─────────────────❒`)
+        if (!results.length) return m.reply(`🍓 *『 𝐁𝐎𝐓 𝐒𝐓𝐑𝐀𝐖𝐁𝐄𝐑𝐘 』* 🍓
+❌ *SIN RESULTADOS*
 
-        let txt = `╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
-│ 🔍 *RESULTADOS DE BÚSQUEDA*
-│
-│ 🌙 *Consulta:* ${text}
-╰─────────────────❒\n\n`
+💖 *No se encontró nada sobre:* ${text}`)
+
+        let txt = `🍓 *『 𝐁𝐎𝐓 𝐒𝐓𝐑𝐀𝐖𝐁𝐄𝐑𝐑𝐘 』* 🍓
+🔍 *RESULTADOS DE BÚSQUEDA*
+
+💖 *Consulta:* ${text}\n\n`
 
         txt += results.map((v, i) => {
-            return `╭─── 🌩️ *RESULTADO ${i + 1}* ───╮
-│ 📌 *${v.title}*
-│ 📝 ${v.description}
-│ 🔗 ${v.url}
-╰───────────────────╯`
+            return `🍓─── *RESULTADO ${i + 1}* ───🍓
+📌 *${v.title}*
+📝 ${v.description}
+🔗 ${v.url}`
         }).join('\n\n')
 
-        txt += `\n\n> *“La sabiduría del trueno nocturno”* ⚡\n> *© Team Nightwish*`
+        txt += `\n\n> *“Strawberry encontró la info más dulce”* 🍓\n> *© Bot Strawberry*`
 
         await conn.reply(m.chat, txt, m)
         await m.react('✅')
@@ -42,12 +38,11 @@ let handler = async (m, { conn, text }) => {
     } catch (e) {
         console.error(e)
         await m.react('❌')
-        m.reply(`╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
-│ ⛈️ *ERROR*
-│
-│ ⚡ *Falló la búsqueda*
-│ 🌙 *Intenta de nuevo*
-╰─────────────────❒`)
+        m.reply(`🍓 *『 𝐁𝐎𝐓 𝐒𝐓𝐑𝐀𝐖𝐁𝐄𝐑𝐘 』* 🍓
+💖 *ERROR*
+
+🍓 *Falló la búsqueda*
+💖 *Intenta de nuevo*`)
     }
 }
 
