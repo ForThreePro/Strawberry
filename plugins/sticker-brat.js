d45df47dffe596891a4369ad6e932c1336121b44
YALLICO OWNER <yallico2024@gmail.com>
2026-07-12 16:52:54 -0500

    Actualizar sticker-brat.js

--- plugins/sticker-brat.js
+++ plugins/sticker-brat.js
@@ -4,18 +4,18 @@ import { exec } from 'child_process'
 
 var handler = async (m, { conn, text, usedPrefix, command }) => {
     let final = text ? text.trim() : (m.quoted?.text || null)
-    if (!final) return conn.reply(m.chat, `⚡ *Escribe el contenido para tu sticker*\n\n> *Ejemplo:* ${usedPrefix + command} Hola mundo`, m)
+    if (!final) return conn.reply(m.chat, `🍓 *Escribe el contenido para tu sticker*\n\n> *Ejemplo:* ${usedPrefix + command} Hola fresita`, m)
 
     if (final.length > 35) {
-        return conn.reply(m.chat, `⚠️ *Demasiado largo.*\n\n📌 Máximo: *35 letras*`, m)
+        return conn.reply(m.chat, `⚠️ *Demasiado largo.*\n\n🍓 Máximo: *35 letras*`, m)
     }
 
-    await m.react('🕒')
+    await m.react('⏳')
 
     try {
         const formatted = wrap(final, 28)
         const key = Buffer.from('c3lscGh5LTZmMTUwZA==', 'base64').toString('utf-8')
-        const url = `https://sylphyy.xyz/tools/brat?text=${encodeURIComponent(formatted)}&color=black&fondo=white&type=Nose&api_key=${key}`
+        const url = `https://sylphyy.xyz/tools/brat?text=${encodeURIComponent(formatted)}&color=pink&fondo=white&type=Nose&api_key=${key}`
 
         const res = await axios.get(url, { responseType: 'arraybuffer' })
 
@@ -32,11 +32,11 @@ var handler = async (m, { conn, text, usedPrefix, command }) => {
 
         await conn.sendMessage(m.chat, { 
             sticker: fs.readFileSync(webp), 
-            packname: "Barboza Developer 👤", 
-            author: "Dev Barboza x Zona Developers ⚡" 
+            packname: "Bot Strawberry 🍓", 
+            author: "Whois Yallico 💖" 
         }, { quoted: m })
 
-        await m.react('✔️')
+        await m.react('✅')
 
         if (fs.existsSync(img)) fs.unlinkSync(img)
         if (fs.existsSync(webp)) fs.unlinkSync(webp)
