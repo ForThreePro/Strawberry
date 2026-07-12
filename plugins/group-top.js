45d76637919ba46a1f711c9e5deb002abc213df3
YALLICO OWNER <yallico2024@gmail.com>
2026-07-12 16:49:42 -0500

    Actualizar group-top.js

--- plugins/group-top.js
+++ plugins/group-top.js
@@ -4,11 +4,10 @@ import path from 'path'
 let user = a => '@' + a.split('@')[0]
 
 function handler(m, { groupMetadata, command, conn, text, usedPrefix}) {
-if (!text) return m.reply(`╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
-│ 🏆 *TOP 10*
-│
-│ ⚡ *Ejemplo:*.top gente mas activa
-╰─────────────────❒`)
+if (!text) return m.reply(`🍓 *『 𝐁𝐎𝐓 𝐒𝐓𝐑𝐀𝐖𝐁𝐄𝐑𝐘 』* 🍓
+💖 *TOP 10*
+
+🍓 *Ejemplo:*.top gente mas activa`)
 
 let ps = groupMetadata.participants.map(v => v.id)
 let a = ps.getRandom()
@@ -22,26 +21,24 @@ let h = ps.getRandom()
 let i = ps.getRandom()
 let j = ps.getRandom()
 let k = Math.floor(Math.random() * 70);
-let x = `${pickRandom(['⛈️','🌩️','⚡','🌙','🔥','💀','😎','🤙','👑','💥'])}`
+let x = `${pickRandom(['🍓','💖','👑','🌟','✨','🎀','🍰','😋','🔥','💥'])}`
 let vn = `https://hansxd.nasihosting.com/sound/sound${k}.mp3`
 
-let top = `╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
-│ 🏆 *TOP 10: ${text.toUpperCase()}*
-│
-│ 👑 *1.* ${user(a)}
-│ 👑 *2.* ${user(b)}
-│ 👑 *3.* ${user(c)}
-│ ⚡ *4.* ${user(d)}
-│ ⚡ *5.* ${user(e)}
-│ ⚡ *6.* ${user(f)}
-│ ⚡ *7.* ${user(g)}
-│ ⚡ *8.* ${user(h)}
-│ ⚡ *9.* ${user(i)}
-│ ⚡ *10.* ${user(j)}
-│
-│ > *“Que el trueno elija a los mejores”*
-╰─────────────────❒`
+let top = `🍓 *『 𝐁𝐎𝐓 𝐒𝐓𝐑𝐀𝐖𝐁𝐄𝐑𝐘 』* 🍓
+💖 *TOP 10: ${text.toUpperCase()}*
 
+👑 *1.* ${user(a)}
+👑 *2.* ${user(b)}
+👑 *3.* ${user(c)}
+🍓 *4.* ${user(d)}
+🍓 *5.* ${user(e)}
+🍓 *6.* ${user(f)}
+🍓 *7.* ${user(g)}
+🍓 *8.* ${user(h)}
+🍓 *9.* ${user(i)}
+🍓 *10.* ${user(j)}
+
+> *“Que las fresas elijan a los mejores”* ${x}`
 m.reply(top, null, { mentions: [a, b, c, d, e, f, g, h, i, j]})
 }
 
