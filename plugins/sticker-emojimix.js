2d868bb3c6cad2a39593a601da7c0b7d56c1f0ac
YALLICO OWNER <yallico2024@gmail.com>
2026-07-12 16:54:14 -0500

    Actualizar sticker-emojimix.js

--- plugins/sticker-emojimix.js
+++ plugins/sticker-emojimix.js
@@ -1,14 +1,14 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     let [emoji1, emoji2] = text.split(/[&+\s]+/)
-    if (!emoji1 || !emoji2) return conn.reply(m.chat, `⚡ *RAYO PREM* ➔ Uso correcto: ${usedPrefix + command} emoji1+emoji2\n> Ejemplo: ${usedPrefix + command} 😃+⚡`, m) // Cambiado
+    if (!emoji1 || !emoji2) return conn.reply(m.chat, `🍓 *BOT STRAWBERRY* ➔ Uso correcto: ${usedPrefix + command} emoji1+emoji2\n> Ejemplo: ${usedPrefix + command} 😃+🍓`, m)
 
     let url = `https://api.evogb.org/tools/emojimix?emoji1=${encodeURIComponent(emoji1)}&emoji2=${encodeURIComponent(emoji2)}&key=sasuke`
 
     try {
         await conn.sendMessage(m.chat, { sticker: { url: url } }, { quoted: m })
-        await conn.react(m.chat, '🌙', m.key) // React extra
+        await conn.react(m.chat, '💖', m.key) // React extra
     } catch (e) {
-        conn.reply(m.chat, `⚡ *RAYO PREM ERROR* ➔ Falló al mezclar los emojis.\n\`\`${e.message}\`\``, m) // Cambiado
+        conn.reply(m.chat, `🍓 *BOT STRAWBERRY ERROR* ➔ Falló al mezclar los emojis.\n\`\`${e.message}\`\``, m)
     }
 }
 
