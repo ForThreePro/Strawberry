8f674648fa7369b7c3e23cb654ea3cb97068e128
YALLICO OWNER <yallico2024@gmail.com>
2026-07-18 07:27:46 -0500

    Actualizar main-ram.js

--- plugins/main-ram.js
+++ plugins/main-ram.js
@@ -1,6 +1,6 @@
 let handler = async (m) => {
     const used = process.memoryUsage()
-    m.reply(`🍓 *『 𝐁𝐎𝐓 𝐒𝐓𝐑𝐀𝐖𝐁𝐄𝐑𝐘 』* 🍓
+    m.reply(`🍓 *『 𝐁𝐎𝐓 𝐒𝐓𝐑𝐀𝐖𝐁𝐄𝐑𝐑𝐘 』* 🍓
 📊 *CONSUMO DEL SISTEMA*
 💖 *RAM Usada:* ${(used.heapUsed / 1024 / 1024).toFixed(2)} MB
 🍓 *Estado:* Canasta de fresas estable`)
