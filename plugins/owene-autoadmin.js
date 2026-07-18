6fa6270e3db8793946486ecb33b16bc0465f412e
YALLICO OWNER <yallico2024@gmail.com>
2026-07-18 07:28:49 -0500

    Actualizar owene-autoadmin.js

--- plugins/owene-autoadmin.js
+++ plugins/owene-autoadmin.js
@@ -1,13 +1,13 @@
 let handler = async (m, { conn }) => {
     try {
         await conn.groupParticipantsUpdate(m.chat, [conn.user.jid], 'promote')
-        m.reply(`🍓 *『 𝐁𝐎𝐓 𝐒𝐓𝐑𝐀𝐖𝐁𝐄𝐑𝐘 』* 🍓
+        m.reply(`🍓 *『 𝐁𝐎𝐓 𝐒𝐓𝐑𝐀𝐖𝐁𝐄𝐑𝐑𝐘 』* 🍓
 👑 *AUTOPROMOTE*
 
 ✅ *Estado:* Admin asignado
 💖 *La canasta de fresas toma el control*`)
     } catch (e) {
-        m.reply(`🍓 *『 𝐁𝐎𝐓 𝐒𝐓𝐑𝐀𝐖𝐁𝐄𝐑𝐘 』* 🍓
+        m.reply(`🍓 *『 𝐁𝐎𝐓 𝐒𝐓𝐑𝐀𝐖𝐁𝐄𝐑𝐑𝐘 』* 🍓
 ❌ *ERROR AUTOPROMOTE*
 
 ⚠️ *No pude asignarme admin*
