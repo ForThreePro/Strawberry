64eabb85444c1110cdb0761cf4149a545fea7c01
YALLICO OWNER <yallico2024@gmail.com>
2026-07-18 07:23:52 -0500

    Actualizar group-link.js

--- plugins/group-link.js
+++ plugins/group-link.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
     try {
         let link = await conn.groupInviteCode(m.chat)
-        let text = `🍓 *『 𝐁𝐎𝐓 𝐒𝐓𝐑𝐀𝐖𝐁𝐄𝐑𝐘 』* 🍓
+        let text = `🍓 *『 𝐁𝐎𝐓 𝐒𝐓𝐑𝐀𝐖𝐁𝐄𝐑𝐑𝐘 』* 🍓
 💖 *LINK DEL GRUPO*
 
 🍓 https://chat.whatsapp.com/${link}
@@ -9,7 +9,7 @@ let handler = async (m, { conn }) => {
 > *“Comparte la canasta de fresas”* 🍓`
         m.reply(text)
     } catch {
-        m.reply(`🍓 *『 𝐁𝐎𝐓 𝐒𝐓𝐑𝐀𝐖𝐁𝐄𝐑𝐘 』* 🍓
+        m.reply(`🍓 *『 𝐁𝐎𝐓 𝐒𝐓𝐑𝐀𝐖𝐁𝐄𝐑𝐑𝐘 』* 🍓
 ❌ *ERROR*
 
 🍓 *No pude obtener el link*
